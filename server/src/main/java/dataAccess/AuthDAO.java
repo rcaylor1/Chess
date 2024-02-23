6ec8a345d7af258bb01dd2488bbc29ef5344ee0c
rcaylor1 <rcaylor@byu.edu>
2024-02-22 22:29:21 -0700

    wrote authDAOs idk if they're right

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -4,4 +4,7 @@ import model.AuthData;
 
 public interface AuthDAO {
     public void clear() throws DataAccessException;
+    public AuthData createAuth(String username) throws DataAccessException;
+    public AuthData getAuth(AuthData authToken) throws DataAccessException;
+    public void deleteAuth(AuthData authToken) throws DataAccessException;
 }
--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,11 +1,33 @@
 package dataAccess;
 
 import model.AuthData;
+
+import javax.xml.crypto.Data;
 import java.util.HashMap;
+import java.util.UUID;
+
 public class MemoryAuthDAO implements AuthDAO{
     private final HashMap<String, AuthData> auths = new HashMap<>();
 
     public void clear() throws DataAccessException{
         auths.clear();
+    }
+
+    public AuthData createAuth(String username) throws DataAccessException{
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, username);
+        auths.put(authToken, authData);
+        return authData;
+    }
+
+    public AuthData getAuth(AuthData auth) throws DataAccessException{
+        if (auths.containsKey(auth.username())){
+            return auths.get(auth.username());
+        }
+        return null;
+    }
+
+    public void deleteAuth(AuthData auth) throws DataAccessException{
+        auths.remove(auth.username());
     }
 }
