ce2d6bfc1128b535b233f821c1157988385c4001
rcaylor1 <rcaylor@byu.edu>
2024-02-22 23:45:50 -0700

    wrote gameDAOs idk if they're right. Going to start writing clear test tomorrow but I've been working for too long and I'm tired lol

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,7 +1,12 @@
 package dataAccess;
 
 import model.GameData;
+import java.util.ArrayList;
 
 public interface GameDAO {
     public void clear() throws DataAccessException;
+    public void createGame(GameData game) throws DataAccessException;
+    public GameData getGame(GameData game) throws DataAccessException;
+    public ArrayList<GameData> listGames() throws DataAccessException;
+    public void updateGame(GameData game) throws DataAccessException;
 }
--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -2,11 +2,36 @@ package dataAccess;
 
 import model.GameData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 public class MemoryGameDAO implements GameDAO{
-    private final HashMap<String, GameData> games = new HashMap<>();
+    private final HashMap<Integer, GameData> games = new HashMap<>();
 
     public void clear() throws DataAccessException{
         games.clear();
+    }
+
+    public void createGame(GameData game) throws DataAccessException{
+        for (GameData data : games.values()){
+            if (!game.gameName().equals(data.gameName())){
+                games.put(game.gameID(), game);
+            }
+        }
+        throw new DataAccessException("Game already exists");
+    }
+
+    public GameData getGame(GameData game) throws DataAccessException{
+        return games.getOrDefault(game.gameID(), null);
+    }
+
+    public ArrayList<GameData> listGames() throws DataAccessException{
+        return new ArrayList<>(games.values());
+    }
+
+    public void updateGame(GameData game) throws DataAccessException{
+        if (games.containsKey(game.gameID())){
+            games.put(game.gameID(), game);
+        } else
+            throw new DataAccessException("Unable to update");
     }
 }
