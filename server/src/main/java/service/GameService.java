f77467c45bbf7ee7e151be0a9bcaca3dcca5e097
rcaylor1 <rcaylor@byu.edu>
2024-02-25 18:26:20 -0700

    wrote joinGame service and unit tests which all passed; still working on handlers

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -26,4 +26,27 @@ public class GameService {
         gameDAO.createGame(new GameData(gameID, null, null, gameName, new ChessGame()));
         return gameID;
     }
+
+    public void joinGame(String authToken, String color, int gameID) throws DataAccessException {
+        GameData joinedGame = gameDAO.getGame(gameID);
+        if (gameDAO.getGame(gameID) == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        if (authDAO.getAuth(authToken) == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        if (color != null) {
+            if (color.equals("WHITE")){
+                if (joinedGame.whiteUsername() != null){
+                    throw new DataAccessException("Error: already taken");
+                }
+                gameDAO.updateGame(new GameData(joinedGame.gameID(), authDAO.getAuth(authToken).username(), joinedGame.blackUsername(), joinedGame.gameName(), joinedGame.game()));
+            } else if (color.equals("BLACK")){
+                if (joinedGame.blackUsername() != null){
+                    throw new DataAccessException("Error: already taken");
+                }
+                gameDAO.updateGame(new GameData(joinedGame.gameID(), joinedGame.whiteUsername(), authDAO.getAuth(authToken).username(), joinedGame.gameName(), joinedGame.game()));
+            }
+        }
+    }
 }
---
+++ server/src/test/java/serviceTests/JoinGameTests.java
@@ -0,0 +1,35 @@
+package serviceTests;
+
+import dataAccess.*;
+import model.*;
+import service.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+public class JoinGameTests {
+    @Test
+    void joinGamePositive() throws DataAccessException {
+        AuthDAO testAuthDAO = new MemoryAuthDAO();
+        GameDAO testGameDAO = new MemoryGameDAO();
+        GameService service = new GameService(testGameDAO, testAuthDAO);
+
+        testAuthDAO.createAuth(new AuthData("authToken", "rcaylor"));
+        int gameID = service.createGame("authToken", "gamename");
+
+        Assertions.assertDoesNotThrow(() -> service.joinGame("authToken", "WHITE", gameID));
+    }
+
+    @Test
+    void joinGameNegative() throws DataAccessException {
+        AuthDAO testAuthDAO = new MemoryAuthDAO();
+        GameDAO testGameDAO = new MemoryGameDAO();
+        GameService service = new GameService(testGameDAO, testAuthDAO);
+
+        testAuthDAO.createAuth(new AuthData("authToken", "rcaylor"));
+        testAuthDAO.createAuth(new AuthData("newAuth", "hopefullyalmostdone"));
+
+        int gameID = service.createGame("authToken", "gamename");
+        service.joinGame("authToken", "WHITE", gameID);
+
+        Assertions.assertThrows(DataAccessException.class, () -> service.joinGame("newAuth", "WHITE", gameID));
+    }
+}
