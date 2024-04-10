c98d2a858a502b1f4b8bb1f249f09b63901d4805
rcaylor1 <rcaylor@byu.edu>
2024-04-09 22:08:46 -0600

    finished websocketfacade???

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -2,6 +2,8 @@ package websocket;
 
 import javax.websocket.*;
 
+import chess.ChessGame;
+import chess.ChessMove;
 import ui.ResponseException;
 import webSocketMessages.serverMessages.LoadGame;
 import webSocketMessages.serverMessages.Notification;
@@ -58,5 +60,25 @@ public class WebSocketFacade extends Endpoint{
 
     private void sendMessage(UserGameCommand command) throws IOException{
         session.getBasicRemote().sendText(new Gson().toJson(command));
+    }
+
+    public void joinPlayer(String authToken, int gameID, ChessGame.TeamColor team) throws IOException {
+        this.sendMessage(new JoinPlayer(authToken, gameID, team));
+    }
+
+    public void joinObserver(String authToken, int gameID) throws IOException {
+        this.sendMessage(new JoinObserver(authToken, gameID));
+    }
+
+    public void makeMove(String authToken, int gameID, ChessMove move) throws IOException {
+        this.sendMessage(new MakeMove(authToken, gameID, move));
+    }
+
+    public void leaveGame(String authToken, int gameID) throws IOException {
+        this.sendMessage(new Leave(authToken, gameID));
+    }
+
+    public void resignGame(String authToken, int gameID) throws IOException {
+        this.sendMessage(new Resign(authToken, gameID));
     }
 }
