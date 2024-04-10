69f599e62810d1274934f899f3d69d987ba38ae8
rcaylor1 <rcaylor@byu.edu>
2024-04-09 23:19:33 -0600

    finished websocketSessions???

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,4 +1,17 @@
 package server;
 
+import com.google.gson.Gson;
+import model.AuthData;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.userCommands.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 public class WebSocketHandler {
+
 }
--- server/src/main/java/server/WebSocketSessions.java
+++ server/src/main/java/server/WebSocketSessions.java
@@ -1,4 +1,37 @@
 package server;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class WebSocketSessions {
+    private final ConcurrentHashMap<Integer, HashMap<String, Session>> sessions = new ConcurrentHashMap<>();
+
+    public void addSessionToGame(int gameID, String authToken, Session session) {
+        HashMap<String, Session> newSessions = sessions.get(gameID);
+        if (newSessions == null) {
+            newSessions = new HashMap<>();
+            sessions.put(gameID, newSessions);
+        }
+        newSessions.put(authToken, session);
+    }
+
+    public void removeSessionFromGame(int gameID, String authToken) {
+        HashMap<String, Session> newSession = sessions.get(gameID);
+        if (newSession != null) {
+            newSession.remove(authToken);
+        }
+    }
+
+    public void removeSession(Session session){
+        for (HashMap<String, Session> newSession : sessions.values()) {
+            newSession.values().removeIf(value -> value.equals(session));
+        }
+    }
+
+    public HashMap<String, Session> getSessionsForGame(int gameID) {
+        return sessions.get(gameID);
+    }
+
 }
